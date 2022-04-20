2e97dd7431f7ff7b9958898e954283d4a0e7bebe
mk2 <mk2@mk2.red>
2022-04-20 12:24:30 +0900

    fix: コードフォーマットを追加

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-
-  const assetSource = Image.resolveAssetSource(require('./assets/icon.png'));
+  const assetSource = Image.resolveAssetSource(require("./assets/icon.png"));
 
   useEffect(() => {
-    Image.getSize('https://avatars.githubusercontent.com/u/747840?s=96&v=4', (w, h) => {
-      console.log({w, h});
-    });
+    Image.getSize(
+      "https://avatars.githubusercontent.com/u/747840?s=96&v=4",
+      (w, h) => {
+        console.log({ w, h });
+      }
+    );
   }, []);
 
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/icon.png')} />
-      <Image source={{uri: 'https://avatars.githubusercontent.com/u/747840?s=96&v=4'}} />
+      <Image source={require("./assets/icon.png")} />
+      <Image
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/747840?s=96&v=4",
+        }}
+      />
       <Text>Hello Expo</Text>
     </View>
   );
@@ -23,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
